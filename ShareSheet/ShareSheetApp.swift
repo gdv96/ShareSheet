45657273cef26f7dee8f5dace8d3905e395209eb
Giovanni <giovannidv96@gmail.com>
2021-10-16 10:26:55 +0200

    Initial Commit

---
+++ ShareSheet/ShareSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareSheetApp.swift
+//  ShareSheet
+//
+//  Created by devannogiovanni on 16/10/21.
+//
+
+import SwiftUI
+
+@main
+struct ShareSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
